52395c012ad9e890140ada699dd6edb4fa92c76e
Yusuf Salman Noerinaldy Siregar <33023147+yusufsiregar44@users.noreply.github.com>
2018-04-02 21:32:19 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Scanner LOYO = new Scanner(System.in);
+
+        String masukString = LOYO.next();
+
+        LOYO.close();
+
+        System.out.println(masukString);
+
+        System.out.println("Hello World");
+
+
+    }
+
+}
+
+//class is for holding variables and methods
+
+/* single line
+ */
